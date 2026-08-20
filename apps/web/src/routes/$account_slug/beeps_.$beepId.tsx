057aa2e2aacab5bdbd68ffdf27a456048a23934d
yuler <is.yuler@gmail.com>
2026-08-20 17:37:56 +0800

    🐛 [web] Invalidate router after deleting a beep

--- apps/web/src/routes/$account_slug/beeps_.$beepId.tsx
+++ apps/web/src/routes/$account_slug/beeps_.$beepId.tsx
@@ -65,10 +65,8 @@ function BeepDetailPage() {
 		setError(null);
 		try {
 			await deleteBeep(slug, beep.id);
-			await router.navigate({
-				to: "/$account_slug/beeps",
-				params: { account_slug: slug },
-			});
+			await router.navigate({ to: "/$account_slug/beeps", params: { account_slug: slug } });
+			await router.invalidate();
 		} catch (err) {
 			setDeleting(false);
 			setError(
