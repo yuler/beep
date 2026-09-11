dd62352cb0798a5faffc8e152f01b194e29f6b2a
yuler <is.yuler@gmail.com>
2026-09-11 14:36:40 +0800

    📝 [core] Add TODO markers to unimplemented webhook channel handler

--- core/app/models/channel/webhook.rb
+++ core/app/models/channel/webhook.rb
@@ -1,12 +1,16 @@
+# TODO: implement webhook channel delivery and config validation
 module Channel::Webhook
   module_function
 
   def deliver_beep(channel, beep, run: nil)
+    # TODO: implement webhook delivery
   end
 
   def deliver_test!(channel)
+    # TODO: implement webhook test delivery
   end
 
   def validate_config(channel)
+    # TODO: implement webhook config validation (URL, headers, secret, etc.)
   end
 end
