09842baac52fdffb7d8588b3764e96f20a69116c
yuler <is.yuler@gmail.com>
2026-09-16 17:53:35 +0800

    ♻️ [core] Nest beeps stats resource inside beeps collection routes

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -105,12 +105,12 @@ Rails.application.routes.draw do
       end
 
       # Beep
-      namespace :beeps do
-        resource :stats, only: :show
-      end
       resources :beep_proposals, only: :create
       resources :beeps, only: %i[ index show create update destroy ] do
         scope module: :beeps do
+          collection do
+            resource :stats, only: :show, as: :beeps_stats
+          end
           resource :pause, only: %i[ create destroy ]
           resources :runs, only: %i[ index create ]
         end
